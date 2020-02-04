0e6e1f66a8591f70bd1287e13a68dc75f6242711
isabel k. lee <hello@isabelklee.co>
2020-02-04 21:59:59 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -1,11 +1,36 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
+directors_database
 # Call the method directors_database to retrieve the NDS
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
+  pp nds
   nil
 end
 
+# directors_database = [
+#   {:name=>"Stephen Spielberg",
+#     :movies=> [
+#       {:title=>"Jaws", :studio=>"Universal", :worldwide_gross=>260000000, :release_year=>1975},
+#       {:title=>"Close Encounters of the Third Kind", :studio=>"Columbia", :worldwide_gross=>135189114, :release_year=>1977},
+#       {:title=>"Raiders of the Lost Ark", :studio=>"Paramount",:worldwide_gross=>248159971, :release_year=>1981}
+#     ]
+#   }
+# ]
+
+# directors_database[0] = "Stephen Spielberg"
+# directors_database[0][:movies] = [{:title=>"Jaws", :studio=>"Universal", :worldwide_gross=>260000000, :release_year=>1975}, {:title=>"Close Encounters of the Third Kind", :studio=>"Columbia", :worldwide_gross=>135189114, :release_year=>1977}, {:title=>"Raiders of the Lost Ark", :studio=>"Paramount",:worldwide_gross=>248159971, :release_year=>1981}]
+# directors_database[0][:movies][0] = {:title=>"Jaws", :studio=>"Universal", :worldwide_gross=>260000000, :release_year=>1975}
+# directors_database[0][:movies][0][:title] = "Jaws"
+
 def print_first_directors_movie_titles
+    index = 0 
+    while index < directors_database[0][:movies].length do
+      print "#{directors_database[0][:movies][index][:title]}\n"
+    index += 1
+  end
 end
+
+print_first_directors_movie_titles
