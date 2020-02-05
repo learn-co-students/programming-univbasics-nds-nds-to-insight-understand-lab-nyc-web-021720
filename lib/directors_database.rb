4af77bc2291177020aaabcf40197e9262a8d4db3
Zac Uslianer <zacuslianer@gmail.com>
2020-02-05 22:43:05 +0000

    Done.

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -1,6 +1,18 @@
-require 'yaml'
+require 'pp'
+require 'pry'
 
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
+
+# movies = directors_database[0][:movies]
+
+
+# row_index = 0 
+# while row_index < movies.count do 
+#   puts movies[row_index][:title]
+    
+# row_index += 1
+
+# end
