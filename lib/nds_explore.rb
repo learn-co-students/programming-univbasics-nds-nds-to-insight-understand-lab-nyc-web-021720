942eb8a1ab20209fa29b169f525e08e0fcf40ad5
Johnson Kow <jkow95@gmail.com>
2020-02-17 01:12:15 +0000

    Done.

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -6,15 +6,15 @@ require 'pry'
 
 def pretty_print_nds(nds)
 pp nds
-binding.pry
 end
 
-pp directors_database
 
 def print_first_directors_movie_titles
+  movies = directors_database[0][:movies]#[0][:title]   #JAWS
   
-#  i = 0 
-#  while i < directors_database.length do 
-#    puts "Row #{i} has #{directors_database[i]} columns"
-#  end 
+  i = 0 
+  while i < directors_database[0][:movies].length 
+    puts directors_database[0][:movies][i][:title]
+    i += 1 
+  end 
 end 
